2c6ed246cc8ad70bcb358c02eff50a27582d3fab
alessandro <alessandro.massarella@gmail.com>
2020-11-12 22:23:48 +0100

    jSnack6

---
+++ JSnack-6/main.js
@@ -0,0 +1,31 @@
+// jsnack 6
+// dato questo array di obj iniziale:
+// var arrayObj = [
+//   {name: 'Poppy', type: 'tshirt', color: 'red'},
+//   {name: 'Jumping', type: 'occhiali', color: 'blue'},
+//   {name: 'CrissCross', type: 'scarpe', color: 'black'},
+//   {name: 'Jenny', type: 'borsa', color: 'pink'},
+// ];
+// Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array)
+// una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale
+
+const arrayObj = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
+
+const newArrayObj = arrayObj.map((element) => {
+// creo un array con tutti i caratteri, poi creo una variabile con un numero random che poi assocerò all'indice dell'array di caratteri. Successivamente assegnerò il valore/lettera alla key position di ogni oggetto. 
+  const caratteri = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  let randomNumb = Math.floor(Math.random() * 26);
+
+  console.log('lettera random' + caratteri[randomNumb])
+
+  element.position = caratteri[randomNumb];
+
+  return element;
+})
+console.log(newArrayObj);
