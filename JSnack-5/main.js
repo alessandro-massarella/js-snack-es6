2fde060e91cc87df491f93c9a699a9e1f8893bd0
alessandro <alessandro.massarella@gmail.com>
2020-11-13 10:59:11 +0100

    Fix2

--- JSnack-5/main.js
+++ JSnack-5/main.js
@@ -4,12 +4,24 @@
 // chiediamo all’utente con 2 prompt due numeri tra 0 e la lunghezza dell’array
 // quindi creiamone uno nuovo con solo i valori che hanno la posizione compresa tra i due numeri inseriti dall’utente
 
+// -------------------PRIMA STESURA----------------------
+// const amiciArr = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+// console.log(amiciArr);
+//
+// const firstNum = parseInt(prompt('inserisci un numero tra 0 e 5'));
+// const secondNum = parseInt(prompt('inserisci un numero tra 0 e 5'));
+//
+// const newAmici = amiciArr.slice(firstNum, secondNum);
+// console.log(newAmici);
 
+
+// -------------------------correzione-------
 const amiciArr = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
-console.log(amiciArr);
 
-const firstNum = parseInt(prompt('inserisci un numero tra 0 e 5'));
-const secondNum = parseInt(prompt('inserisci un numero tra 0 e 5'));
+const firstNum = parseInt(prompt('inserisci un numero tra 0 e' + (amiciArr.length-1)));
 
-const newAmici = amiciArr.slice(firstNum, secondNum);
+const secondNum = parseInt(prompt('inserisci un numero tra' + firstNum + ' e ' + (amiciArr.length-1)));
+
+const newAmici = amiciArr.filter((myElement, index) => index <= secondNum && index >= secondNum);
+// -------------------------------------------------
 console.log(newAmici);
